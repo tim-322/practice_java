77fa972f6277d1d457772b7d106bb0528b6a4ec3
tim-322 <94117409+tim-322@users.noreply.github.com>
2023-08-03 09:12:19 +0800

    Update CodeComparator.java
    fix some bugs

--- src/main/java/com/hooper/CodeComparator.java
+++ src/main/java/com/hooper/CodeComparator.java
@@ -1,24 +1,60 @@
 package com.hooper;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class CodeComparator {
 
     private static final Map<String, String> CONSTANTS;
-    private static final String CONSTANT_FILE_PATH = "/Users/felix/IdeaProjects/practice/src/main/java/com/hooper/CommonConstant.java";
+    private static final String ROL_CONSTANT_FILE_PATH = "D:\\project\\Base\\zdbatch-boot\\zdbatch-bid\\zdbatch-common\\src\\main\\java\\com\\ylsoft\\core\\server\\common\\constant\\RolConstants.java";
+    private static final String CONSTANT_FILE_PATH = "D:\\project\\Base\\zdbatch-boot\\zdbatch-bid\\zdbatch-common\\src\\main\\java\\com\\ylsoft\\core\\server\\common\\constant\\CommonConstant.java";
+    private static final String OUT_FILE = "D:\\project\\常量修改后不一致统计.md";
 
     static {
-        CONSTANTS = readConstantsFromFile(CONSTANT_FILE_PATH);
+        CONSTANTS = readConstantsFromFile(ROL_CONSTANT_FILE_PATH);
+        CONSTANTS.putAll(readConstantsFromFile(CONSTANT_FILE_PATH));
+        CONSTANTS.forEach((k,v) -> System.out.println(k + "=" +v));
     }
 
     public static void main(String[] args) throws Exception {
-        List<String> diffList = getDiffFromConstants("/Users/felix/IdeaProjects/practice/src/main/java/com/hooper/TestConstantOrg.java",
-                "/Users/felix/IdeaProjects/practice/src/main/java/com/hooper/TestConstantModified.java");
-        diffList.forEach(System.out::println);
+        new File(OUT_FILE).delete();
+        String baseModPath = "D:\\project\\Base\\zdbatch-boot\\";
+        String baseOrgPath = "D:\\project\\bat\\";
+        String commonPath = "zdbatch-bid/zdbatch-preSaleHouse/src/main/java/com/ylsoft/core/server";
+        String orgDir = baseOrgPath + commonPath;
+        String modifiedDir = baseModPath + commonPath;
+        List<String> orgPathList = getFilesByDir(orgDir, ".java").stream().map(File::getAbsolutePath).collect(Collectors.toList());
+        List<String> modifiedPathList = getFilesByDir(modifiedDir, ".java").stream().map(File::getAbsolutePath).collect(Collectors.toList());
+        if (orgPathList.size() != modifiedPathList.size()) {
+            throw new RuntimeException("文件数量不一致");
+        }
+        for (int i = 0; i < orgPathList.size(); i++) {
+            List<String> diffFromConstants = getDiffFromConstants(orgPathList.get(i), modifiedPathList.get(i));
+            writeList(diffFromConstants, OUT_FILE, modifiedPathList.get(i));
+        }
+        System.out.println("done");
+    }
+
+    private static void writeList(List<String> list, String path, String filePath) {
+        if (list.isEmpty()) {
+            return;
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path, true))) {
+            writer.write("## " + filePath + "\n");
+            writer.write("```java\n");
+            list.forEach(s -> {
+                try {
+                    writer.write(s);
+                    writer.newLine();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            writer.write("```\n");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static Map<String, String> readConstantsFromFile(String filePath) {
@@ -38,7 +74,11 @@ public class CodeComparator {
                         String constantName = line.split(" ")[4];
                         String constantValue = line.split("= ")[1].trim().replace(";", "");
                         String key = currentClassName + "." + constantName;
+                        int preSize = constantsMap.size();
                         constantsMap.put(key, constantValue);
+                        if (preSize == constantsMap.size()) {
+                            System.err.println("Duplicate constant: " + key);
+                        }
                     }
                 }
             }
@@ -69,7 +109,7 @@ public class CodeComparator {
         while (orgIdx < orgLines.size() && modifiedIdx < modifiedLines.size()) {
             String org = orgLines.get(orgIdx++);
             String modified = modifiedLines.get(modifiedIdx++);
-            if (org.equals(modified)) continue;
+            if (org.equals(modified) || contentSame(org,modified)) continue;
             Set<String> modifiedConstSet = getConstantFromLine(modified);
             if (modifiedConstSet.isEmpty()) {
                 diffList.add(modified + "-->" + org);
@@ -78,6 +118,12 @@ public class CodeComparator {
             System.out.println("替换前:" + modified);
             for (String s : modifiedConstSet) {
                 modified = modified.replace(s, CONSTANTS.get(s));
+            }
+            if (modified.contains("RolConstants.")) {
+                modified = modified.replace("RolConstants.", "");
+            }
+            if (modified.contains("CommonConstant.")) {
+                modified = modified.replace("CommonConstant.", "");
             }
             System.out.println("替换后:" + modified);
             if (!contentSame(org, modified)) {
@@ -108,7 +154,7 @@ public class CodeComparator {
         Set<String> constSet = new HashSet<>();
         for (int i = 0; i < line.length(); i++) {
             char c = line.charAt(i);
-            if (Character.isUpperCase(c) || c == '_' || c == '.') {
+            if (Character.isUpperCase(c) || c == '_' || c == '.' || c == '4') {
                 if (c == '.' && bigLetterCount == 0) {
                     continue;
                 }
@@ -119,6 +165,18 @@ public class CodeComparator {
                 if (bigLetterCount > 4) {
                     String constant = line.substring(startIdx, i);
                     if (constant.endsWith(".")) constant = line.substring(startIdx, i - 1);
+                    if (constant.endsWith("_NULL")) {
+                        if (line.charAt(i) == '2') {
+                            constant = constant + "2";
+                        }
+                    }
+                    if (constant.endsWith("UNKNOWN_ERROR_")) {
+                        constant = constant + '2';
+                    }
+                    if (constant.contains("JOB_MAIN_STATUS")||constant.contains("COLLECTION_STATUS")) {
+                        constant = "COMMON_STATUS." + constant.split("\\.")[1];
+                        System.err.println("替换常量:" + constant);
+                    }
                     if (CONSTANTS.containsKey(constant)) {
                         constSet.add(constant);
                     }
@@ -129,4 +187,19 @@ public class CodeComparator {
         return constSet;
     }
 
+    public static List<File> getFilesByDir(String targetDir, String type) {
+        File file = new File(targetDir);
+        List<File> list = new ArrayList<>();
+        File[] files = file.listFiles();
+        for (File f : files) {
+            if (f.isDirectory()) {
+                list.addAll(getFilesByDir(f.getAbsolutePath(), type));
+            } else {
+                if (f.getName().endsWith(type)) {
+                    list.add(f);
+                }
+            }
+        }
+        return list;
+    }
 }
